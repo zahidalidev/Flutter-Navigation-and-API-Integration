fa319530ca6600dfff41cf86704b3949c1ddeb54
zahidalidev <m.zahidalidev@gmail.com>
2021-04-28 19:37:46 +0500

    data loaded and renders from api

--- lib/screens/Assignment3.dart
+++ lib/screens/Assignment3.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter_application_1/Widgets/MyAppBar.dart';
 import 'package:flutter_application_1/Widgets/Drawer.dart';
 
@@ -57,6 +61,16 @@ class _Assignment3 extends State<Assignment3> {
     },
   ];
 
+  // Future<Post> products;
+  Future<List<Product>> products;
+
+  @override
+  void initState() {
+    super.initState();
+    products = getProducts();
+    print(products);
+  }
+
   void dellItem(item, index) async {
     setState(() {
       items.remove(item);
@@ -81,80 +95,93 @@ class _Assignment3 extends State<Assignment3> {
       // imported from PrimeDetect
       body: Container(
         margin: EdgeInsets.only(top: 20),
-        child: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return Dismissible(
-              background: Container(color: Colors.red[300]),
-              key: Key(items[index]['title']),
-              onDismissed: (direction) {
-                dellItem(items[index], index);
-              },
-              child: Card(
-                clipBehavior: Clip.antiAlias,
-                margin:
-                    EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 10),
-                elevation: 3,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 10),
-                      child: Image.asset("lib/assets/smash3.jpg"),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 10, left: 16, right: 16),
-                      child: Text(
-                        items[index]['description'],
-                        style: TextStyle(
-                            color: Colors.black.withOpacity(0.6), fontSize: 15),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 16, right: 10, bottom: 10),
-                      child: ButtonBar(
-                        alignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            items[index]['title'],
-                            style: TextStyle(
-                              color: Colors.teal,
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
+        child: FutureBuilder<List<Product>>(
+          future: products,
+          builder: (context, body) {
+            if (body.hasData) {
+              return ListView.builder(
+                itemCount: body.data.length,
+                itemBuilder: (context, index) {
+                  return Dismissible(
+                    background: Container(color: Colors.red[300]),
+                    key: Key(body.data[index].title),
+                    onDismissed: (direction) {
+                      dellItem(body.data[index], index);
+                    },
+                    child: Card(
+                      clipBehavior: Clip.antiAlias,
+                      margin: EdgeInsets.only(
+                          left: 30, right: 30, top: 10, bottom: 10),
+                      elevation: 3,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 10),
+                            child: Image.asset("lib/assets/smash3.jpg"),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10, left: 16, right: 16),
+                            child: Text(
+                              body.data[index].description,
+                              style: TextStyle(
+                                  color: Colors.black.withOpacity(0.6),
+                                  fontSize: 15),
                             ),
                           ),
-                          Row(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(right: 13),
-                                child: GestureDetector(
-                                  child: Icon(
-                                    Icons.favorite,
-                                    color: Colors.grey,
-                                    size: 25,
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 16, right: 10, bottom: 10),
+                            child: ButtonBar(
+                              alignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  body.data[index].title,
+                                  style: TextStyle(
+                                    color: Colors.teal,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
                                   ),
-                                  onTap: () => null,
                                 ),
-                              ),
-                              GestureDetector(
-                                child: Icon(
-                                  Icons.share,
-                                  color: Colors.grey,
-                                  size: 25,
+                                Row(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(right: 13),
+                                      child: GestureDetector(
+                                        child: Icon(
+                                          Icons.favorite,
+                                          color: Colors.grey,
+                                          size: 25,
+                                        ),
+                                        onTap: () => null,
+                                      ),
+                                    ),
+                                    GestureDetector(
+                                      child: Icon(
+                                        Icons.share,
+                                        color: Colors.grey,
+                                        size: 25,
+                                      ),
+                                      onTap: () => null,
+                                    ),
+                                  ],
                                 ),
-                                onTap: () => null,
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ),
-              ),
-            );
+                  );
+                },
+              );
+            } else if (body.hasError) {
+              return Text("tt:${body.error}");
+            }
+
+            // By default, it show a loading spinner.
+            return CircularProgressIndicator();
           },
         ),
       ),
@@ -188,4 +215,43 @@ Future<ConfirmAction> _asyncConfirmDialog(BuildContext context) async {
       );
     },
   );
+}
+
+// Future<http.Response> getProducts() {
+//   return http.get(
+//       "https://my-json-server.typicode.com/zahidalidev/fakeProduct/products");
+// }
+
+List<Product> parseData(String responseBody) {
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<Product>((json) => Product.fromJson(json)).toList();
+}
+
+Future<List<Product>> getProducts() async {
+  final response = await http.get(
+      "https://my-json-server.typicode.com/zahidalidev/fakeProduct/products");
+
+  if (response.statusCode == 200) {
+    // If the server returns an OK response, then parse the JSON.
+    return parseData(response.body);
+  } else {
+    // If the response was umexpected, throw an error.
+    throw Exception('Failed to load product');
+  }
+}
+
+class Product {
+  final int id;
+  final String title;
+  final String description;
+
+  Product({this.id, this.title, this.description});
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+    );
+  }
 }
