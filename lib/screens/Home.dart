7c1ac378007134facd1b393202bef2573a2798c8
zahidalidev <m.zahidalidev@gmail.com>
2021-05-06 06:49:33 +0500

    :art: format

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Widgets/Heading.dart';
 
 // Zahid Ali Regestration Number 2018-CS-136
 
@@ -21,14 +22,25 @@ class PrimeDetectState extends StatelessWidget {
                       margin:
                           const EdgeInsets.only(top: 20, left: 30, right: 30),
                       alignment: Alignment.centerLeft,
-                      child: Text(
-                        "1. Design Home Screen, add logo, short description of your assignment requirements; add drawer for navigation options to screens, designed in next steps \n 2. Design Add Record Screen & Integrate Web API to add records; Add at least 5 records \n3. Design List Records. Integrate Web API to fetch records and then display them in tabular form, in this screen",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.cyan,
-                          fontWeight: FontWeight.w400,
-                        ),
+                      child: Column(
+                        children: [
+                          Container(
+                            alignment: Alignment.centerLeft,
+                            margin: const EdgeInsets.only(bottom: 20),
+                            child: Heading(
+                              label: "Assignment Description",
+                            ),
+                          ),
+                          Text(
+                            "1. Design Home Screen, add logo, short description of your assignment requirements; add drawer for navigation options to screens, designed in next steps \n\n2. Design Add Record Screen & Integrate Web API to add records; Add at least 5 records \n\n3. Design List Records. Integrate Web API to fetch records and then display them in tabular form, in this screen",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
